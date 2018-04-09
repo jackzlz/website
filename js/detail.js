72ab01f4e2c56b6bd1e03e1d874c2761add2919b
jackzlz <jackzlz@126.com>
2018-04-09 22:58:37 +0800

    增加数字输入框

--- js/detail.js
+++ js/detail.js
@@ -1,6 +1,32 @@
 
 var v = new Vue({
-    el: '#app'
+    el: '#app',
+
+    data: {
+        num_actual: 1,
+        min: 1,
+        max: 10
+    },
+
+    computed: {
+        num: {
+            get: function () {
+                return this.num_actual;
+            },
+            set: function (newValue) {
+                if (!isNaN(newValue)) {
+                    this.num_actual = newValue < this.min ? this.min : newValue > this.max ? this.max : newValue;
+                }
+            }
+        }
+    },
+    methods: {
+
+        buy: function (event) {
+            event.preventDefault();
+            alert()
+        }
+    }
 
 
 });  
